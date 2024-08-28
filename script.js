8f7eaf5624a59a21f3bbdf21f70d1d7bad9edd77
Jacob Jansson <41388531+jaken1234@users.noreply.github.com>
2024-08-28 09:59:06 +0200

    adds

---
+++ script.js
@@ -0,0 +1,47 @@
+const socket = new SockJS("http://localhost:8080/websocket");
+const messageList = document.getElementById("messageList")
+const sendText = document.getElementById("sendText")
+const sendBtn = document.getElementById("sendBtn")
+const loginForm = document.getElementById("loginForm")
+const loginBtn = document.getElementById("loginBtn")
+
+const stompClient = new Stomp.over(socket);
+
+stompClient.connect({},(frame) => {
+    console.log("connected :)");
+    stompClient.subscribe("/topic/greetings", (greeting) => {
+        console.log(JSON.parse(greeting.body).content)
+
+        let li = document.createElement("li");
+        li.innerText = JSON.parse(greeting.body).content;
+        messageList.appendChild(li)
+
+    })
+
+    stompClient.subscribe("/topic/chat", (chat) => {
+        console.log(JSON.parse(chat.body).chat)
+
+        let li = document.createElement("li");
+        li.innerText = JSON.parse(chat.body).chat;
+        messageList.appendChild(li)
+    })
+
+    sendHello("Jacob")
+})
+
+function sendHello(name) {
+    stompClient.send("/app/hello", {}, JSON.stringify({"name": name}))
+}
+
+sendBtn.addEventListener("click", () => {
+    stompClient.send("/app/chat", {}, JSON.stringify({"content": sendText.value}))
+    sendText.innerHTML="";
+})
+
+loginBtn.addEventListener("click", () => {
+    console.log("loggar in")
+})
+
+function login() {
+    
+}
