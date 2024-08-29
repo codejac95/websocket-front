c45ea7602da3918f7069433c6311664e6b479015
Kribbz0r <kribbz0r@gmail.com>
2024-08-29 11:50:34 +0200

    Woopido

--- script.js
+++ script.js
@@ -2,12 +2,16 @@ const socket = new SockJS("https://dolphin-app-eqkxi.ondigitalocean.app/websocke
 const messageList = document.getElementById("messageList")
 const sendText = document.getElementById("sendText")
 const sendBtn = document.getElementById("sendBtn")
-const loginForm = document.getElementById("loginForm")
-const loginBtn = document.getElementById("loginBtn")
+const createUserNameForm = document.getElementById("createUserNameForm")
+const createPasswordForm = document.getElementById("createPasswordForm")
+const createUserBtn = document.getElementById("createUserBtn")
+const loginUsernameForm = document.getElementById("loginUsernameForm")
+const loginPasswordForm = document.getElementById("loginPasswordForm")
+const loginUserBtn = document.getElementById("loginUserBtn")
 
 const stompClient = new Stomp.over(socket);
 
-stompClient.connect({},(frame) => {
+stompClient.connect({}, (frame) => {
     console.log("connected :)");
     stompClient.subscribe("/topic/greetings", (greeting) => {
         console.log(JSON.parse(greeting.body).content)
@@ -30,33 +34,56 @@ stompClient.connect({},(frame) => {
 })
 
 function sendHello(name) {
-    stompClient.send("/app/hello", {}, JSON.stringify({"name": name}))
+    stompClient.send("/app/hello", {}, JSON.stringify({ "name": name }))
 }
 
 sendBtn.addEventListener("click", () => {
-    stompClient.send("/app/chat", {}, JSON.stringify({"content": sendText.value}))
-    sendText.innerHTML="";
+    stompClient.send("/app/chat", {}, JSON.stringify({ "content": sendText.value }))
+    sendText.innerHTML = "";
 })
 
-loginBtn.addEventListener("click", () => {
-    login();
+createUserBtn.addEventListener("click", () => {
+    createUser();
 })
 
-const object = JSON.stringify({
-    "username": loginForm.value,
-    "password": "password"
-  });
+loginUserBtn.addEventListener("click", () => {
+    loginUser();
+})
 
-    function login() {
-        fetch("https://dolphin-app-eqkxi.ondigitalocean.app/createUser",  {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-            }, body: JSON.stringify({username:loginForm.value,password:"password"})
+function loginUser() {
+    fetch("https://dolphin-app-eqkxi.ondigitalocean.app/loginUser", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Access-Control-Allow-Origin": "*",
+        }, body: JSON.stringify({ username: loginUsernameForm.value, password: loginPasswordForm.value })
+    })
+
+        .then(response => response.json())
+
+
+
+        .then(data => {
+            console.log(data);
+            if (data.username === loginUsernameForm.value && data.password === loginPasswordForm.value)
+                alert(data.username + " har loggat in")
+            else alert("Fel uppgifter")
+
         })
-            .then(response => response.json())
-            .then(data => {
-                alert(data.username+" är skapad")
-                
-            })
-        }
+}
+
+function createUser() {
+    fetch("https://dolphin-app-eqkxi.ondigitalocean.app/createUser", {
+        method: "POST",
+        headers: {
+
+
+            "Content-Type": "application/json",
+        }, body: JSON.stringify({ username: createUserNameForm.value, password: createPasswordForm.value })
+    })
+        .then(response => response.json())
+        .then(data => {
+            alert(data.username + " är skapad")
+
+        })
+}
